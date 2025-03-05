c4a56365b9a5484e97b901d3fa383b17b1f830af
akhil soigama <akhilsoigama@gmail.com>
2025-03-05 18:20:48 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  basePath: '/public',
+ 
 
   async rewrites() {
 
     return [
-      
-        {
-          source: '/api/:path*',
-          destination: 'http://localhost:5000//api/:path*', 
-        },
-      ];
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/api/:path*',
+      },
+    ];
+
   },
 };
 
