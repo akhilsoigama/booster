12c33cb22b5322c55e0b10e840114e4971162c6a
akhil soigama <akhilsoigama@gmail.com>
2025-03-05 17:54:31 +0530

    add vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
+  reactStrictMode: true,
 
   async rewrites() {
 
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: 'https://booster-f0cy2omok-akhilsoigamas-projects.vercel.app/api/:path*',
       },
     ];
 
