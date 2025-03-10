428946c45f3ed381c11041631b83835f6ab4fb78
akhil soigama <akhilsoigama@gmail.com>
2025-03-10 16:11:06 +0530

    create post

--- src/app/Server/controller/postController/postController.js
+++ src/app/Server/controller/postController/postController.js
@@ -1,5 +1,22 @@
-const Post = require('../../../model/post/post');
+const Post = require("../../../model/post/post");
 
 const Posts = async (req, res) => {
-    const { title, content, auther, image, User_id } = req.body
-}
+  const { title, content, auther, image, User_id } = req.body; 
+
+  try {
+    const createPost = new Post({
+      title,
+      content,
+      auther, 
+      image,
+      User_id,
+    });
+
+    await createPost.save();
+    res.status(201).json({ message: "Post has been created", createPost });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = Posts;
